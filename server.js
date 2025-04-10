18e614043b0d726ea87818a419260b8e611b1b19
camilo <juan20ruiz19@gmail.com>
2025-04-10 01:07:40 -0500

    Actualizamos server.js

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-const express = require('express');
 const db = require('./src/config/db');
-const app = express();
+const app = require('./src/app');
+
 const PORT = 3000;
 
-// Middleware para parsear JSON
-app.use(express.json());
-
-// Importar rutas
-app.use('/api/clientes', require('./src/routes/cliente.routes'));
-app.use('/api/menu', require('./src/routes/menu.routes'));
-app.use('/api/pedidos', require('./src/routes/pedido.routes'));
-app.use('/api/detalles', require('./src/routes/detallepedido.routes'));
-
-
-// Conectar a la base de datos
 db.authenticate()
-  .then(() => console.log('✅ Conexión a la base de datos exitosa'))
-  .catch(err => console.error('❌ Error al conectar a la base de datos', err));
-
-// Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+  .then(() => {
+    console.log('✅ Conexión a la base de datos exitosa');
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error('❌ Error al conectar a la base de datos:', err));
