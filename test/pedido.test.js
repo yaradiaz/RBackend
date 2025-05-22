c5c720511f167e9d24479f407c3c24cca385ce4e
Yara Diaz <yaradiaz@unicomfacauca.edu.co>
2025-05-21 23:48:42 -0500

    actualizacion para test en pedido

--- test/pedido.test.js
+++ test/pedido.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../src/app');
+const app = require('../src/app'); // o '../server' si lo haces así
 const db = require('../src/config/db');
 
 describe('Pruebas de rutas de Pedido', () => {
@@ -7,9 +7,42 @@ describe('Pruebas de rutas de Pedido', () => {
     const res = await request(app).get('/api/pedidos');
     expect(res.statusCode).toBe(200);
   });
-
-  afterAll(async () => {
-    await db.close();
-  });
 });
 
+describe('Pruebas protegidas de pedidos con JWT', () => {
+  let token;
+
+  beforeAll(async () => {
+    const login = await request(app)
+      .post('/api/login')
+      .send({
+        correo: 'cliente1@example.com',
+        contrasena: '123456'
+      });
+
+    token = login.body.token;
+  });
+
+  test('No debe permitir crear pedido sin token', async () => {
+    const res = await request(app)
+      .post('/api/pedidos')
+      .send({ total: 15000, cliente_id_cliente: 1 });
+
+    expect(res.statusCode).toBe(403);
+    expect(res.body.mensaje).toBe('Token no proporcionado');
+  });
+
+  test('Debe permitir crear pedido con token válido', async () => {
+    const res = await request(app)
+      .post('/api/pedidos')
+      .set('Authorization', token)
+      .send({ total: 15000, cliente_id_cliente: 1 });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty('id_pedido');
+  });
+
+  afterAll(async () => {
+    await db.close(); // Cierra conexión solo una vez
+  });
+});
