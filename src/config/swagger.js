803f89c143e8dc2a8a0b1135fbe472638fa5e657
Yara Diaz <yaradiaz@unicomfacauca.edu.co>
2025-05-24 10:09:55 -0500

    Actualizacion swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,16 +1,33 @@
+// Generación del archivo swagger.json con la documentación
+swaggerAutogen()(outputFile, endPointsFiles, doc);
+
+
 import swaggerAutogen from 'swagger-autogen';
 
-const outputFile = './swagger.json'; 
-const endPointsFiles = ['./src/app.js']; 
+const outputFile = './swagger.json';
+const endPointsFiles = ['./src/app.js'];
 
 const doc = {
-    info: {
-        title: 'API Restaurante', 
-        description: 'Esta API permite gestionar lun restaurante',  
-    },
-    host: 'localhost:3000', 
-    schemes: ['http'],  
+  info: {
+    title: 'API Restaurante',
+    description: 'API para la gestión de clientes, menús, pedidos y autenticación.',
+  },
+  host: 'localhost:3000',
+  schemes: ['http'],
+  tags: [
+    { name: 'Clientes', description: 'Operaciones relacionadas con clientes' },
+    { name: 'Menú', description: 'Gestión de productos del menú' },
+    { name: 'Pedidos', description: 'Registro de pedidos' },
+    { name: 'Detalles de Pedido', description: 'Detalle de productos por pedido' },
+    { name: 'Autenticación', description: 'Login y seguridad con JWT' },
+  ],
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    }
+  }
 };
 
-// Generación del archivo swagger.json con la documentación
 swaggerAutogen()(outputFile, endPointsFiles, doc);
