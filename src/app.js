80855d310608859ce285b42dcfe0a47ad179a957
Yara Diaz <yaradiaz@unicomfacauca.edu.co>
2025-05-24 11:03:27 -0500

    Se agrega mensaje en app.js y se modifica un error en sync.js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Ruta raíz para comprobar que el servidor está activo
+app.get('/', (req, res) => {
+  res.send('¡Servidor funcionando correctamente en la raíz!');
+});
+
 // Rutas
 app.use('/api/clientes', require('./routes/cliente.routes'));
 app.use('/api/menu', require('./routes/menu.routes'));
 app.use('/api/pedidos', require('./routes/pedido.routes'));
 app.use('/api/detalles', require('./routes/detallepedido.routes'));
-app.use('/api/login', require('./src/routes/auth.routes'));
+app.use('/api/login', require('./routes/auth.routes'));
 
 module.exports = app;
--- sync.js
+++ sync.js
@@ -4,7 +4,8 @@ const db = require('./src/config/db');
 require('./src/models/cliente.model');
 require('./src/models/menu.model');
 require('./src/models/pedido.model');
-require('./src/models/detallepedidos.model');
+require('./src/models/detallePedido.model');
+
 
 
 const syncDB = async () => {
